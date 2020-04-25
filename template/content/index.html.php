89bd00d2acb9b30ad3c3b9637875cc3a5a421af8
christophesd <christophe.sd@gmail.com>
2020-04-25 15:23:25 +0200

    ajout model bootstrap pour btm suppr (js)

--- template/content/index.html.php
+++ template/content/index.html.php
@@ -9,7 +9,9 @@
 
     <?php if (!empty($_SESSION["id_utilisateur"])) {?>
         <p>Bonjour <strong><?=$prenom?></strong> ! Vous êtes bien connecté !</p>
-        <button onClick="del()" type="button" class="btn btn-outline-danger">Supprimer mon compte</button>
+        <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModalCenter">
+            Supprimer mon compte
+        </button>
     <?php } else {?>
         <p>Vous n'êtes pas connecté.</p>
     <?php } ?>
@@ -17,11 +19,30 @@
 </div>
 
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Voulez vous supprimer votre compte ?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        L'intégralité de votre compte sera supprimé sans possibilité de retour en arrière. Nous serons dans l'incapacité de vous restitier vos données.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Revenir sur mon Compte</button>
+        <button type="button" onClick="del()" class="btn btn-primary">Supprimer</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <script>
     function del() {
-        if(confirm("Voulez vous vraiment supprimer votre compte ?"))
-        {
-            document.location.href="index.php?controller=utilisateur&task=del";
-        }
+        document.location.href="index.php?controller=utilisateur&task=del";
     }
 </script>
